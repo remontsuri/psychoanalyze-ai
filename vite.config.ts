0ccfc58f603c9aaf5cf79ae30ae37526dc1973dc
Your Name <clocekrindo@yandex.ru>
2025-12-15 23:18:12 +0300

    Fix white screen - simplify build

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          charts: ['recharts'],
-          utils: ['html2canvas', 'jspdf', 'xlsx']
-        }
-      }
-    }
+    assetsDir: 'assets',
+    sourcemap: false
   }
 })
